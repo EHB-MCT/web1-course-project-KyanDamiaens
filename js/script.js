b868edcc827ff3291e395a89672cfbc55a9523b3
KyanDamiaens <kyan.deprins@student.ehb.be>
2026-04-20 15:57:23 +0200

    eerste versie's

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+toggle.addEventListener("click", function () {
+    nav.classList.toggle("show");
+});
